e4615d8be9209a565885461546b40252f87befbd
anna Penchansky <annapenchansky@annas-MacBook-Pro.local>
2017-11-19 15:09:22 +0200

    have full functionality. you can now filter a films by date, and name, and add to your cart. You can also track how much is in your budget.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
 var app = angular.module("anguFlix", []);
 
 // app.config(function($stateProvider, $urlRouteProvider, $locationProvider){
-
+//   $locationProvider.html5Mode(true);
+//   $stateProvider
+//   .state('home', {
+//     url:'/home',
+//     controller: 'mainCtrl',
+//     templateUrl: '/templates/home.html'
+//   })
 // })
